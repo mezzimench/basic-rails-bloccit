316a4db0886c32dbd10fb034c379536f58904551
mezzimench <henry.archer500@gmail.com>
2015-03-06 07:29:21 -0700

    Merging create-posts-controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/contact'
 
-  get 'welcome/index'
+  resources :posts
 
-  get 'welcome/about'
+  get 'about' => 'welcome#about'
 
     root to: 'welcome#index'
 
