ccebdcb6ebfa8ffb873675f6aebb2d3650aac048
mezzimench <henry.archer500@gmail.com>
2015-03-19 09:33:57 -0600

    Authentication assignment update for merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312160338) do
+ActiveRecord::Schema.define(version: 20150319023234) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20150312160338) do
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
