bacda85d5bb6709199d76ff620b980d3d30f4b03
mezzimench <henry.archer500@gmail.com>
2015-03-18 20:24:56 -0600

    Complete authentication continued assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  #before_action :flash_attack
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def flash_attack
+   flash[:notice] = "This is a flash_attack!"
+  end
     
-    before_action :configure_permitted_parameters, if: :devise_controller?
+    
 
     protected
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+
+  skip_before_action :flash_attack, :only => [:index, :new]
+ 
   def index
     @posts = Post.all
   end
@@ -11,17 +14,16 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-      def  create
-        @post = Post.new(params.require(:post).permit(:title, :body))
-      
-        if @post.save
-          flash[:notice] = "Post was saved."
-          redirect_to @post
-        else
-          flash[:error] = "There was an error saving the post. Please try again."
-          render :new
-        end
-      end
+  def  create
+    @post = Post.new(params.require(:post).permit(:title, :body))
+    if @post.save
+      flash[:notice] = "Post was saved."
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
 
   def edit
   end
