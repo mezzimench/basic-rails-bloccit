beda11af0f5ce771902648945c01a918324b771a
mezzimench <henry.archer500@gmail.com>
2015-03-20 15:56:10 -0600

    Complete Associations assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,10 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :advertisements
   belongs_to :user
-
+  scope :ordered_by_title, -> {Post.where(title: :asc)}
+  scope :ordered_by_reverse_created_at, -> {Post.where(created_at: :desc)}
   default_scope {order('created_at DESC')}
+
+
 end
 
